fb59538322158f075dc698ce6d5318f8b4f19905
unknown <MICHAELSIBANDA>
2024-11-06 19:05:15 +0200

    first commit

---
+++ Radio app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RadioContainer from './components/RadioContainer';
+
+const App = () => {
+  return (
+    <div>
+      <RadioContainer />
+    </div>
+  );
+};
+
+export default App;
